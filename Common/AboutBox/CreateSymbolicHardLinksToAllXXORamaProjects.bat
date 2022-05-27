58915d30daba41bb4d331d12eeeed8ba811db71e
Doctor Wizard <dev@wizster.com>
2022-05-27 18:37:00 -0400

    Update copyright, urls, and other info
    Checking this in now in case Visual Studio screws it up, again

---
+++ Common/AboutBox/CreateSymbolicHardLinksToAllXXORamaProjects.bat
@@ -0,0 +1,60 @@
+:: Blank-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\BlankORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\BlankORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\BlankORama\frmAbout.resx
+:: Channel-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\ChannelORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\ChannelORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\ChannelORama\frmAbout.resx
+:: Compare-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\CompareORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\CompareORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\CompareORama\frmAbout.resx
+:: Dim-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\DimORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\DimORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\DimORama\frmAbout.resx
+:: Flip-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\FlipORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\FlipORama4\ChannelORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\FlipORama\frmAbout.resx
+:: InfO-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\InfORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\InfORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\InfORama\frmAbout.resx
+:: Launch-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\LaunchORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\LaunchORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\LaunchORama\frmAbout.resx
+:: Map-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\MapORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\MapORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\MapORama\frmAbout.resx
+:: Merge-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\MergeORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\MergeORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\MergeORama\frmAbout.resx
+:: RGB-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\RGBORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\RGBORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\RGBORama\frmAbout.resx
+:: Sort-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\SortORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\SortORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\SortORama\frmAbout.resx
+:: Sparkle-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\SparkleORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\SparkleORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\SparkleORama\frmAbout.resx
+:: Split-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\SplitORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\SplitORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\SplitORama\frmAbout.resx
+:: Time-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\TimeORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\TimeORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\TimeORama\frmAbout.resx
+:: Vamp-O-Rama
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.cs W:\Documents\SourceCode\Windows\UtilORama4\VampORama\frmAbout.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.Designer.cs W:\Documents\SourceCode\Windows\UtilORama4\VampORama\frmAbout.Designer.cs
+mklink /D W:\Documents\SourceCode\Windows\UtilORama4\Common\AboutBox\frmAbout.resx W:\Documents\SourceCode\Windows\UtilORama4\VampORama\frmAbout.resx
